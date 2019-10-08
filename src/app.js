dfffe5cbdd5c529820a1907b711e6a892418909d
Samuel Sousa <samuelsousapereira@gmail.com>
2019-10-08 16:54:50 -0300

    First Commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    cors = require('cors'),
+    helmet = require('helmet');
+
+const mainRoute = require('./routes/route');
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(mainRoute);
+
+app.listen(3000);
